b191dc7145d16d7c2f6361d2a6fc97dabb6c0970
Daniel Parent <danielparent21@gmail.com>
2020-09-07 17:39:33 -0400

    js exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  let C = (F - 32) * (5/9);
+  C = Math.round(C * 10)/10;
+  return C;
 }
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let F = (C * (9/5)) + 32;
+  F = Math.round(F * 10)/10;
+  return F;
 }
 
 module.exports = {
