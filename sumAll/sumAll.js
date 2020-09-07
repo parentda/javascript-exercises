439df395a28109fabedc2f57fa63e367fdd6e706
Daniel Parent <danielparent21@gmail.com>
2020-09-07 17:09:14 -0400

    js exercises

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let x;
+    let y;
+    let sum = 0;
+    if (typeof(start) !== "number" || typeof(stop) !== "number") {
+        return "ERROR";
+    }
+    else if (start < 0 || stop < 0){
+        return "ERROR";
+    }
+    else {
+        if (stop < start) {
+            x = stop;
+            y = start;
+        }
+        else {
+            x = start;
+            y = stop;
+        }
+        for (let i = x; i <= y; i++) {
+            console.log("i: " + i);
+            sum += i;
+            console.log("sum: " + sum);
+        }
+    }
+    console.log(sum);
+    return sum;
 
 }
 
