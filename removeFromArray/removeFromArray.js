e75a9dd754aed1de419775685e440fc45712e5ab
Daniel Parent <danielparent21@gmail.com>
2020-09-07 16:43:50 -0400

    js exercises

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    let arrayLen = array.length;
+    let theArgsLen = theArgs.length;
 
+    let removed;
+
+    for (i = (arrayLen - 1); i >= 0; i--) {
+        for (j = 0; j < theArgsLen; j++) {
+            if (array[i] === theArgs[j]) {
+                removed = array.splice(i, 1);
+                console.log(array);
+                console.log(removed);
+            }
+        }
+    }
+
+    return array;
 }
 
 module.exports = removeFromArray
